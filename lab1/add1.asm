e7465d7a7b20971c4694f8bd24ebe05937ef75b9
Manik Goyal <Manik-Goyal@users.noreply.github.com>
2018-01-27 05:23:02 +0530

    #copied majorly

---
+++ lab1/add1.asm
@@ -0,0 +1,9 @@
+.text
+main:
+# code starts from the line below
+
+li     $t0, 1     # temp reg 1 
+li     $t1, 2     # temp reg 2
+
+# v0 - reg for return value
+add     $v0, $t0, $t1   
---
+++ lab1/add2.asm
@@ -0,0 +1,13 @@
+.text
+main:
+
+# code starts from the line below
+
+lui     $t0, 0x1000     # temporary register t0 = 0x10000000
+ori     $t0, 0x0001     # t0 = 0x10000000 | 0x0001 = 0x10000001
+
+lui     $t1, 0x2000
+ori     $t1, 0x0002
+
+# v0 - register for return value
+add     $v0, $t0, $t1   # v0 = t0 + t1 = 0x30000003
